2699449f781d48ea3c6f7e25cd682994f03450d8
tufangxu <tufanx@uw.edu>
2017-01-12 14:03:55 -0800

    Add files via upload

---
+++ exercise.R
@@ -0,0 +1,31 @@
+# Create a variable `lyric` that contains the text "I like to eat apples and bananas"
+lyric <- "I like to eat apples and bananas"
+ 
+# Use the `substr()` function to extract the 1st through 13th letters from the `lyric`
+# Use `?substr` to see more about this function
+# Store the result in a variable called `intro`
+?substr()
+intro <- substr(lyric,1,13)
+
+
+# Use the `substr()` function to extract the 15th through the last letter of `lyric`
+# Hint: use `nchar()` to determine how many letters there are!
+# Store the result in a variable called `fruits`
+fruits <- substr(lyric,15,nchar(lyric))
+
+# Use the `gsub()` function to substitute all the "a"s in `fruits` with "ee".
+# Hint: see http://www.endmemo.com/program/R/sub.php for a simpmle example (or use `?gsub`)
+# Store the result in a variable called `fruits.e`
+?gsub() 
+fruits.e <- gsub("a","e",fruits)
+  
+# Use the `gsub()` function to substitute all the "a"s in `fruits` with "o".
+# Store the result in a variable called `fruits.o`
+fruits.o <- gsub("a","o",fruits)
+
+# Create a new variable `lyric.e` that is the `intro` combined with the new `fruits.e` ending
+# Print out this variable
+lyrics.e <- paste(intro, fruits.e,sep = " ")
+print(lyrics.e)
+# Print out the `intro` combined with the new `fruits.o` ending
+paste(intro,fruits.o,sep = " ") 
